dd5da807889f5599f9d72ec9ec11b345184f0abd
Andy Sturrock <8336404+andysturrock@users.noreply.github.com>
2024-01-19 16:54:38 +0000

    Check roles in token are appropriate for the action

--- lambda-src/ts-src/handleSlackAtlasDataAuthorizer.ts
+++ lambda-src/ts-src/handleSlackAtlasDataAuthorizer.ts
@@ -5,10 +5,8 @@ import {generatePolicy, validateToken} from './authorization';
 import {getSecretValue} from './awsAPI';
 import {JwtPayload} from 'jsonwebtoken';
 
-// export async function handleSlackAtlasDataAuthorizer(event: APIGatewayTokenAuthorizerEvent): Promise<APIGatewayAuthorizerResult> {
 export async function handleSlackAtlasDataAuthorizer(event: APIGatewayRequestAuthorizerEvent): Promise<APIGatewayAuthorizerResult> {
   try {
-    console.log(`event: ${util.inspect(event, true, 99)}`);
     // The jwks uri comes from the tenant and client id of the app registration for the back end/API.
     // It's this app registration that has issued and signed the JWT.
     const tenantId = await getSecretValue('OrgChartSync', 'tenantId');
@@ -19,18 +17,36 @@ export async function handleSlackAtlasDataAuthorizer(event: APIGatewayRequestAut
     // The audience is the target of the JWT - in this case the client ID of the app registration for the API.
     const audience = backEndClientId;
 
-    // const token = event.headers.authorizationToken.replace('Bearer ', '');
     if(event.headers) {
-      const token = event.headers["Authorization"]?.replace('Bearer ', '');
+      // Seems to sometimes have uppercase A, sometimes not.
+      let token = event.headers["authorization"]?.replace('Bearer ', '');
+      if(!token) {
+        token = event.headers["Authorization"]?.replace('Bearer ', '');
+      }
       if(!token) {
         throw new Error("Missing Authorization header");
       }
+
+      let effect: "Deny" | "Allow" = "Deny";
       const validToken = await validateToken(token, jwksUri, issuer, audience);
-      const scp = (validToken?.payload as JwtPayload)["scp"] as string;
-      const scopes = scp.split(/ /);
-      console.log(`scopes: ${util.inspect(scopes, true, 99)}`);
-      console.log(`validToken: ${util.inspect(validToken, true, 99)}`);
-      const effect = validToken ? "Allow" : "Deny";
+      if(validToken) {
+        const roles = (validToken?.payload as JwtPayload)["roles"] as string[];
+        // Check this token has the appropriate role for the http method
+        switch(event.httpMethod) {
+        case "POST":
+        case "PATCH":
+        case "DELETE": {
+          effect = roles.find((role) => role === "SlackAtlasWriters") ? "Allow" : "Deny";
+          break;
+        }
+        case "GET":{
+          effect = roles.find((role) => role === "SlackAtlasReaders") ? "Allow" : "Deny";
+          break;
+        }
+        default:
+          break;
+        }
+      }
       const policy = generatePolicy('user', effect, event.methodArn);
 
       console.log(`Returning policy: ${util.inspect(policy, true, 99)}`);
