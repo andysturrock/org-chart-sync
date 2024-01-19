7bc23398a360e691533ceca5752bbaa354aa3879
Andy Sturrock <8336404+andysturrock@users.noreply.github.com>
2024-01-19 16:55:25 +0000

    Move AAD section to its own file

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,16 +1,10 @@
 import {useState} from 'react';
-import {graphAPIScopes} from './authConfig';
-import {AADManagerData, getAADHierarchy} from './graph';
-
 import {AuthenticatedTemplate, UnauthenticatedTemplate, useMsal} from '@azure/msal-react';
-
 import './App.css';
-
-import Button from 'react-bootstrap/Button';
-import {SilentRequest} from '@azure/msal-browser';
 import {PageLayout} from './components/PageLayout';
 import {FileSection, FileUser} from './components/FileSection';
 import {SlackAtlasUser, SlackSection} from "./components/SlackSection";
+import {AADSection, AADUser} from './components/AADSection';
 
 /**
 * Renders information about the signed-in user or a button to retrieve data about the user
@@ -27,51 +21,13 @@ function ProfileContent() {
   );
 }
 
-function AADHierarchyContent() {
-  const {instance, accounts} = useMsal();
-  const [managerData, setManagerData] = useState<AADManagerData[]>();
-
-  async function AADHierarchyData() {
-    const silentRequest: SilentRequest = {
-      scopes: graphAPIScopes.scopes,
-      account: accounts[0]
-    };
-    // Silently acquires an access token which is then attached to a request for MS Graph data
-    const authenticationResult = await instance.acquireTokenSilent(silentRequest);
-    const managerData = await getAADHierarchy(authenticationResult.accessToken);
-    setManagerData(managerData);
-  }
-
-  return (
-    <>
-      <hr />
-      <h5 className="card-title">AAD data</h5>
-      <br />
-      {managerData ? (
-        <label>
-        Org stuff
-          <textarea
-            name="postContent"
-            readOnly={true}
-            rows={4} cols={40}
-            value={"AAD Stuff here"}/>
-        </label>
-      ) : (
-        // eslint-disable-next-line @typescript-eslint/no-misused-promises
-        <Button variant="secondary" onClick={AADHierarchyData}>
-          Get AAD Hierarchy Information
-        </Button>
-      )}
-    </>
-  );
-}
-
 /**
 * If a user is authenticated the ProfileContent component above is rendered. Otherwise a message indicating a user is not authenticated is rendered.
 */
 function MainContent() {
   const [slackAtlasUsers, setSlackAtlasUsers] = useState<Map<string, SlackAtlasUser>>();
   const [fileUsers, setFileUsers] = useState<Map<string, FileUser>>();
+  const [azureActiveDirectoryUsers, setAzureActiveDirectoryUsers] = useState<Map<string, AADUser>>();
   return (
     <div className="App">
       <AuthenticatedTemplate>
@@ -83,9 +39,21 @@ function MainContent() {
           </center>
         </h5>
         <ProfileContent />
-        <FileSection fileUsers={fileUsers} setFileUsers={setFileUsers} slackAtlasUsers={slackAtlasUsers}/>
-        <AADHierarchyContent />
-        <SlackSection slackAtlasUsers={slackAtlasUsers} setSlackAtlasUsers={setSlackAtlasUsers} fileUsers={undefined}/>
+        <FileSection
+          fileUsers={fileUsers}
+          setFileUsers={setFileUsers}
+          slackAtlasUsers={slackAtlasUsers}
+        />
+        <AADSection
+          azureActiveDirectoryUsers={azureActiveDirectoryUsers}
+          setAzureActiveDirectoryUsers={setAzureActiveDirectoryUsers}
+        />
+        <SlackSection
+          slackAtlasUsers={slackAtlasUsers}
+          setSlackAtlasUsers={setSlackAtlasUsers}
+          fileUsers={fileUsers}
+          azureActiveDirectoryUsers={azureActiveDirectoryUsers}
+        />
       </AuthenticatedTemplate>
 
       <UnauthenticatedTemplate>
